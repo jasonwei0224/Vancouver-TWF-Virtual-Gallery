4eeab5012c4eab8c34a6edeadc65095f373eb237
jasonwei0224 <45651276+jasonwei0224@users.noreply.github.com>
2020-07-31 14:24:35 +0800

    fix quote error

--- src/ch/Sky-ch/sky_v1-ch.js
+++ src/ch/Sky-ch/sky_v1-ch.js
@@ -192,7 +192,7 @@ class SkyV1_ch extends Component {
       <FormConsent_ch inputId="checkbox-3" consent="我同意台灣文化節將我所拍攝的照片作為「頭頂同一片天計畫」節目的內容與相關宣傳使用。"></FormConsent_ch>
 
 
-        <ButtonCh onclick={this.submitForm} title="送出" togoUrl="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fgallery.vancouvertaiwanfest.ca%2Fsky-ch&amp;src=sdkpreparse""></ButtonCh>
+        <ButtonCh onclick={this.submitForm} title="送出" togoUrl="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fgallery.vancouvertaiwanfest.ca%2Fsky-ch&amp;src=sdkpreparse"></ButtonCh>
 
 
       </Form>
